08d5205bd6af12ce8f2099b815dc57001a55642e
sahana <sahanavuyyala03@gmail.com>
2025-01-09 22:37:05 -0600

    changedsomefiles

--- src/main/java/dsAlgoUtils/ConfigReader.java
+++ src/main/java/dsAlgoUtils/ConfigReader.java
@@ -1,4 +1,5 @@
 package dsAlgoUtils;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -9,12 +10,13 @@ public class ConfigReader {
 
 		//this method is used to load the properties from config.properties file.
 		//and it will return property prop object
-		private Properties prop;
-		public Properties init_prop() {
-			prop = new Properties();
+		
+		public static Properties initializeprop() {
+			Properties prop = new Properties();
+			File profile = new File("C:\\SahanaNUMPYNINJASDET\\gitsss\\DsAlgoQuesters\\src\\test\\resources\\dsAlgoProperties\\Config.properties");
 			try {
-				FileInputStream ip = new FileInputStream("./src/test/resources/dsAlgoProperties/Config.properties");
-				prop.load(ip);
+				FileInputStream fis = new FileInputStream(profile);
+				prop.load(fis);
 			} catch (FileNotFoundException e) {
 				
 				e.printStackTrace();
--- src/test/java/dsAlgoHooks/Hooks.java
+++ src/test/java/dsAlgoHooks/Hooks.java
@@ -12,33 +12,33 @@ import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 
-
 public class Hooks {
 
-	private WebDriver driver ;
+	private WebDriver driver;
 	private DriverFactory driverfactory;
 	private ConfigReader configReader;
 	Properties prop;
-	
-	
+
 	@Before(order=0)
 	public void getProperty() {
-		configReader= new ConfigReader(); 
-		prop =configReader.init_prop();
-			}
+		configReader = new ConfigReader();
+		prop = configReader.initializeprop();
+	}
+
 	@Before(order=1)
 	public void launchBrowser() {
-		String browserName=prop.getProperty("browser");
+		String browserName = prop.getProperty("browser");
 		driverfactory = new DriverFactory();
-		driver=driverfactory.Intializebrowser(browserName);
+		driver = driverfactory.Intializebrowser(browserName);
+		DriverFactory.getdriver();
 		driver.get(prop.getProperty("URL"));
 	}
-	
-	@After(order=0)
+
+   @After(order=0)
 	public void quitBrowser() {
 		driver.quit();
 	}
-	
+
 	@After(order=1)
 	public void tearDown(Scenario scenario) {
     if(scenario.isFailed()) {
@@ -47,7 +47,8 @@ public class Hooks {
     	byte[] sourcePath =((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
     	scenario.attach(sourcePath, screanshotName, screanshotName);
     }
+	
+	
 
 	}
-	
 }
