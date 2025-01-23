c0ace42af6b930d93ab732a81994f4ebbe8291f5
shaalini <shaalinisethumadhavan@gmail.com>
2025-01-23 22:47:49 +0530

    pushing changes

--- src/test/java/dsAlgoRunner/RunnerTest.java
+++ src/test/java/dsAlgoRunner/RunnerTest.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
 
-		features = "src/test/resources/dsAlgoFeatures/Register.feature", 
+		features = "src/test/resources/dsAlgoFeatures/DataStructure.feature", 
 		glue = { "dsAlgoStepdefinition", "dsAlgoHooks" }, 
 		plugin = { "pretty", "html:target/cucumber-reports.html", 
 				"json:target/cucumber-reports.json" },
